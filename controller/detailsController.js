d6eea372efffa9db767cd7d098fe3e5c4f66bbcb
hjkhalid <hk810773@gmail.com>
2024-08-13 12:28:17 +0530

    update in env file

--- controller/detailsController.js
+++ controller/detailsController.js
@@ -1,4 +1,6 @@
 const UserDetails = require("../models/UserDetails");
+const User = require("../models/User");
+const { ObjectId } = require("mongodb"); // Ensure you import ObjectId if not already done
 
 exports.saveUserDetails = async (req, res) => {
   try {
@@ -9,3 +11,28 @@ exports.saveUserDetails = async (req, res) => {
     res.status(400).json({ msg: error.message }); // Changed status to 400 for bad request errors
   }
 };
+
+// exports.userProfession = async (req, res) => {
+//     const { _id } = req.params;
+//     const { profession } = req.body;
+  
+//     // Validate that _id is a valid ObjectId
+//     // if (!ObjectId.isValid(_id)) {
+//     //   return res.status(400).json({ msg: "Invalid user ID format" });
+//     // }
+  
+//     try {
+//       const user = await User.findById(new ObjectId(_id));
+//       if (!user) {
+//         return res.status(404).json({ msg: "User not found" });
+//       }
+  
+//       // Update the user's profession field
+//       user.profession = profession; // Assuming the User schema has a profession field
+//       await user.save(); // Save the updated user
+  
+//       res.status(200).json({ msg: "The profession has been set" });
+//     } catch (error) {
+//       res.status(500).json({ msg: error.message });
+//     }
+//   };
--- routes/savingDetailsRoutes.js
+++ routes/savingDetailsRoutes.js
@@ -1,11 +1,14 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
-const {saveUserDetails} = require('../controller/detailsController')
+const auth = require("../middleware/auth");
+const {
+  saveUserDetails,
+  userProffesion,
+} = require("../controller/detailsController");
 
-//  routes to save the user data in  Database 
+//  routes to save the user data in  Database
 
-router.post('/userDetails',auth,saveUserDetails);
+router.post("/userDetails", auth, saveUserDetails);
+// router.post(`/proffesion`, auth, userProffesion);
 
 module.exports = router;
