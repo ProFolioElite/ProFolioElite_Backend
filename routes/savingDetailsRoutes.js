67636140fe21831f351c6100445ac69cfb129512
hjkhalid <hk810773@gmail.com>
2024-08-29 00:36:01 +0530

    added the getuserdetails

--- routes/savingDetailsRoutes.js
+++ routes/savingDetailsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const {
-    createUserProfile
+    createUserProfile,getUserProfileDetails
 } = require("../controller/detailsController");
 
 //  routes to save the user data in  Database
@@ -11,6 +11,6 @@ router.post("/userDetails", auth, createUserProfile);
 // router.post(`/proffesion`, auth, userProffesion);
 // fetching userDetails 
 
-router.get('/getuserdetails',auth,)
+router.post('/getuserdetails',auth,getUserProfileDetails)
 
 module.exports = router;
